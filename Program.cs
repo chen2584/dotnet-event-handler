712795b58a67332d08cb215617f2bd999838e2c9
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-11-11 15:49:01 +0700

    first init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EventHandler
+{
+    public delegate int dgPointer(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adder a = new Adder();
+            dgPointer pAdder = new dgPointer(a.Add);
+            int iAnswer = pAdder(4, 3);
+            Console.WriteLine("iAnswer = {0}", iAnswer);
+            // Returns “iAnswer = 7”
+        }
+    }
+
+    public class Adder
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Dotnet Event Handler
+
+Ref: https://www.intertech.com/Blog/c-sharp-tutorial-understanding-c-events/
