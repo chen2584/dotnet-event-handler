70eff95e32e1d02d6d29feab46399432c105d12c
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-11-11 15:59:38 +0700

    add example 2

--- Program.cs
+++ Program.cs
@@ -5,21 +5,35 @@ namespace EventHandler
     public delegate int dgPointer(int a, int b);
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Adder a = new Adder();
-            dgPointer pAdder = new dgPointer(a.Add);
-            int iAnswer = pAdder(4, 3);
+            a.OnMultipleOfFiveReached += new Adder.dgEventRaiser(a_MultipleOfFiveReached);
+
+            int iAnswer = a.Add(4, 3);
             Console.WriteLine("iAnswer = {0}", iAnswer);
-            // Returns “iAnswer = 7”
+            iAnswer = a.Add(4, 6);
+            Console.WriteLine("iAnswer = {0}", iAnswer);
+            Console.ReadKey();
+        }
+
+        static void a_MultipleOfFiveReached()
+        {
+            Console.WriteLine("Multiple of five reached!");
         }
     }
 
     public class Adder
     {
+        public delegate void dgEventRaiser();
+        public event dgEventRaiser OnMultipleOfFiveReached;
+
         public int Add(int x, int y)
         {
-            return x + y;
+            int iSum = x + y;
+            if ((iSum % 5 == 0) && (OnMultipleOfFiveReached != null))
+            { OnMultipleOfFiveReached(); }
+            return iSum;
         }
     }
 }
