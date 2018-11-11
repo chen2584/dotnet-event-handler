1717851eb5542f3c0f35cffab12a56686d30846d
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2018-11-11 16:26:17 +0700

    add example 3 (simple event handler)

--- Program.cs
+++ Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 
-namespace EventHandler
+namespace TestEventHandler
 {
-    public delegate int dgPointer(int a, int b);
     class Program
     {
         static void Main()
         {
             Adder a = new Adder();
-            a.OnMultipleOfFiveReached += new Adder.dgEventRaiser(a_MultipleOfFiveReached);
-
+            a.OnMultipleOfFiveReached += a_MultipleOfFiveReached;
             int iAnswer = a.Add(4, 3);
             Console.WriteLine("iAnswer = {0}", iAnswer);
             iAnswer = a.Add(4, 6);
@@ -17,23 +15,23 @@ namespace EventHandler
             Console.ReadKey();
         }
 
-        static void a_MultipleOfFiveReached()
+        static void a_MultipleOfFiveReached(object sender, EventArgs e)
         {
+            //Console.WriteLine(sender is Adder); // true
             Console.WriteLine("Multiple of five reached!");
         }
     }
+}
 
-    public class Adder
+public class Adder
+{
+    public event EventHandler OnMultipleOfFiveReached;
+
+    public int Add(int x, int y)
     {
-        public delegate void dgEventRaiser();
-        public event dgEventRaiser OnMultipleOfFiveReached;
-
-        public int Add(int x, int y)
-        {
-            int iSum = x + y;
-            if ((iSum % 5 == 0) && (OnMultipleOfFiveReached != null))
-            { OnMultipleOfFiveReached(); }
-            return iSum;
-        }
+        int iSum = x + y;
+        if ((iSum % 5 == 0) && (OnMultipleOfFiveReached != null))
+        { OnMultipleOfFiveReached(this, EventArgs.Empty); }
+        return iSum;
     }
 }
